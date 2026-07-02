69d07832aab03a148ace45b8fcdde5920342c6b0
Khushimehta-hub <khushim1626@gmail.com>
2026-07-02 12:59:02 +0530

    ch hash

---
+++ 05_Hashing/CharacterHashing.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class CharacterHashing
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int [] hash = new int[26];
+        for (int i = 0 ; i < s.length() ; i++)
+        {
+            char ch = s.charAt(i);
+            hash[ch - 'a']++;
+        }
+
+        int q = sc.nextInt();
+        while(q-->0)
+        {
+            char c = sc.next().charAt(0);
+            System.out.println("c:" + hash[c-'a']);
+        }
+
+    }
+}
